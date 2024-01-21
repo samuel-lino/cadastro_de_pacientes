4eb3a0a1f3f0e6ab0082ee492d02e25813d5755e
samuel-eduardo <samuellino2006@gmail.com>
2024-01-21 08:51:15 -0300

    funções concluídas

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import $ from 'jquery';
 import './App.css';
 import Head from './componentes/head/index';
-import Modal from './componentes/modal/index.js';
 const url = 'http://localhost:3333/pacientes';
 function App() {
   const [modal, setmodal] = useState([])
@@ -22,52 +21,184 @@ function App() {
       console.error('Erro na requisição:', error);
     });
   });
-  const content = dados.map(paciente => (
-    <div className='info_bio' key={paciente.cpf}>
-      <h2>{paciente.nome}</h2>
-      <h4>{paciente.cpf}</h4>
-      <h4>{paciente.nascimento}</h4>
-      <div className='alterar'>
+  const [vacina, setvacinas] = useState([])
+  const [remedios , setremedios] = useState([])
+  const vac = vacina.map(vacinas=>(
+    <div className='info_data'>
+    <p id={vacinas.nome + '*' + vacinas.data}> Vacina: {vacinas.nome} <span>aplicada em: {vacinas.data}</span><button onClick={(event)=>{
+      var pai = event.currentTarget.parentElement;
+      var pai1 = pai.parentElement;
+      var dado = pai1.querySelector('p').getAttribute('id');
+      var dividir = dado.split('*');
+      var dados = {nome: dividir[0], data: dividir[1]};
+      var newvacinas = vacina.filter(vac => vac.nome !== dados.nome || vac.data !== dados.data);
+      setvacinas(newvacinas)
+    }}>excluir</button></p>
+    
+    </div>
+    ))
+    const conteudo = remedios.map(medicamentos =>(
+      <div className='info_data'>
+      <p id={medicamentos}>{medicamentos}<button className='exclude'  onClick={(event)=>{
+        var pai = event.currentTarget.parentElement;
+        var pai1 = pai.parentElement;
+        var dado = pai1.querySelector('p').getAttribute('id');
+        var newremedios = remedios.filter(vac => vac !== dado );
+        setremedios(newremedios)
+      }}>excluir</button></p>
+      
+      </div>
+      ))
+      const content = dados.map(paciente => (
+        <div className='info_bio' key={paciente.cpf}>
+        <h2>{paciente.nome}</h2>
+        <h4>{paciente.cpf}</h4>
+        <h4>{paciente.nascimento}</h4>
+        <div className='alterar'>
         <button  id={paciente._id} onClick={(event)=>{
           var id = event.currentTarget.getAttribute('id');
           console.log(id)
           deletar(id);
         }
-        }>Deletar</button>
+      }>Deletar</button>
+      <button id={paciente._id} onClick={(event)=>{
+        var id = event.currentTarget.getAttribute('id');
+        setmodal('Editar')
+        var paciente = dados.filter(item=>item._id === id)
+        Editar(id, paciente)
+        setremedios(paciente[0].medicamentosdeusocontinuo)
+        setvacinas(paciente[0].dosesdevacina)
+      }}>Editar</button>
       </div>
-    </div>
-  ));
-  return (
-    <div className="App">
-      <header className="App-header">
+      </div>
+      ));
+      return (
+        <div className="App">
+        <header className="App-header">
         <Head/>
         <button className='criar' onClick={()=>{
           $('.modal').css('display', 'flex');
-            
-          setmodal(<Modal id = '' tipo = 'Adicionar' />)
+          
+          setmodal('Adicionar')
         }}>Novo paciente</button>
         <div className='info'>
-          <div className='modal' style={{display:'none'}}>
-            {modal}
-          </div>
-          <div className='lista'>
-            <h1>lista de pacientes</h1>
-            {content}
-          </div>
-          
-          
+        <div className='modal' style={{display:'none'}}>
+        <div className='formulario' id={modal}>
+        <h4>{modal}</h4>
+        <div className='edit'></div>
+        <div className='dados'>
+        <label id='nome'>Nome:</label>
+        <input className='nome'></input>
         </div>
-
-      </header>
+        <div className='dados'>
+        <label id='cpf'>CPF:</label>
+        <input className='cpf'></input>
+        </div>
+        <div className='dados'>
+        <label id='nascimento'>Data de nascimento:</label>
+        <input type='date' className='nascimento'></input>
+        </div>
+        <div className='dados'>
+        <label id='sexo'>sexo: </label>
+        <label id='sexo'>Masculino</label>
+        <input className='sexo' type='radio' value={'Masculino'} name='sexo'></input>
+        <label id='sexo'>Feminino</label>
+        <input className='sexo' type='radio' value={'Feminino'} name='sexo'></input>
+        </div>
+        <div className='dados'>
+        <label id='sus'>SUS:</label>
+        <input className='sus'></input>
+        </div>
+        <div className='dados'>
+        <label id='hipertenso'>Hipertenso: </label>
+        <label id='hipertenso'>sim</label>
+        <input className='hipertenso' type='radio' value={true} name='hipertenso'></input>
+        <label id='hipertenso'>não</label>
+        <input className='hipertenso' type='radio' value={false} name='hipertenso'></input>
+        </div>
+        <div className='dados'>
+        <label id='diabetico'>Diabetico: </label>
+        <label id='diabetico'>sim</label>
+        <input className='diabetico' type='radio' value={true} name='diabetico'></input>
+        <label id='diabetico'>não</label>
+        <input className='diabetico' type='radio' value={false} name='diabetico'></input>
+        </div>
+        <div className='dados'>
+        <label>Medicamentos</label>
+        <input className='medicamentos'></input><button className='addremedio' onClick={()=>{
+          var ds = $('.medicamentos').val()
+          addremedio(ds, remedios)
+        }
+      }>adicionar</button>
+      <div className='text_scrol'>
+      {conteudo}
+      </div>
+      </div>
+      <div className='dados'>
+      <label>Vacinas</label>
+      <input className='vacinas'></input><input className='datavacina' type='date'></input><button className='addvacina' onClick={()=>{
+        var data = $('.datavacina').val()
+        var nomevac = $('.vacinas').val()
+        var newdata = data.split('-', 3)
+        var vacinas = {nome:nomevac, data:newdata[2]+'/'+newdata[1]+'/'+newdata[0]}
+        var jaexiste = vacina.some(object=>{
+            return object.nome === vacinas.nome && object.data === vacinas.data
+        })
+        if(jaexiste){
+            window.alert('vacina ja adicionada')
+        }else{
+            vacina.push(vacinas)
+        }
+        console.log(vacina)
+      }
+    }>adicionar</button>
+    <div className='text_scrol'>
+    {vac}
     </div>
-  );
-}
-
-export default App;
-
-
-function  deletar(params){
-  fetch(url +'/'+ params, {method:'DELETE'})
+    </div>
+    <div>
+    <button className='modal_salvar' onClick={()=>{
+      if($('.formulario').attr('id') === 'Adicionar'){
+        console.log('clicou')
+        criar(vacina, remedios)
+        setmodal('')
+        Limpar()
+      }
+      else if($('.formulario').attr('id') === 'Editar'){
+        var id = $('.edit').attr('id')
+        console.log(id)
+        editando(id, vacina, remedios)
+      }
+    }} 
+    >salvar</button>
+    <button className='modal_sair' onClick={()=>{
+      $('.modal').css('display', 'none')
+      setmodal('')
+      Limpar()
+    }}>sair</button>
+    </div>
+  
+    
+    </div>
+    </div>
+    <div className='lista'>
+    <h1>lista de pacientes</h1>
+    {content}
+    </div>
+    
+    
+    </div>
+    
+    </header>
+    </div>
+    );
+  }
+  
+  export default App;
+  
+  
+  function  deletar(params){
+    fetch(url +'/'+ params, {method:'DELETE'})
     .then(response=>{
       if(!response.ok){
         throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
@@ -79,12 +210,60 @@ function  deletar(params){
     .catch(error => {
       console.error('Erro na requisição:', error);
     });
-  
-}
-/*function atualizarpaciente(paciente){
-  const modal = document.querySelector('.modal')
-  if(modal.style.display === 'none'){
-    modal.style.display = 'flex'
-    tarefa = 'atualizar'
+    
   }
-}*/
+  async function criar(vacina, remedios){
+    var data = $('.nascimento').val()
+    var newdata = data.split('-', 3)
+    var paciente = {nome:$('.nome').val(), nascimento:newdata[2]+'/'+newdata[1]+'/'+newdata[0], cpf:$('.cpf').val(), diabetico:$("input[name='diabetico']:checked").val(), hipertenso:$("input[name='hipertenso']:checked").val(), sexo:$("input[name='sexo']:checked").val(), sus:$('.sus').val(), medicamentosdeusocontinuo:remedios, dosesdevacina:vacina};
+    const response = await fetch(url,{method:'POST', body:JSON.stringify(paciente), headers:{'Content-Type': 'application/json'}})
+    if(!response.ok){
+        throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+    }
+    else{
+        console.log(`ok ${paciente}`)
+    }
+    $('.modal').css('display', 'none')
+}
+function addremedio(remedio, lista){
+    if(lista.includes(remedio)){
+        window.alert('remedio ja adicionado')
+    }else{
+        lista.push(remedio)
+        $('.medicamentos').val('')
+    }
+}
+
+async function Editar(id, dados){
+  $('.modal').css('display', 'flex');
+  $('.edit').attr('id', id)
+  $('.nome').val(dados[0].nome)
+  var data = await dados[0].nascimento
+  var newdata = data.split('/', 3)
+  $('.nascimento').val(newdata[2]+'-'+newdata[1]+'-'+newdata[0])
+  $('.cpf').val(dados[0].cpf)
+  $("input[name='diabetico'][value ='"+ dados[0].diabetico + "']").prop('checked', true);
+  $("input[name='hipertenso'][value ='"+ dados[0].hipertenso + "']").prop('checked', true);
+  $("input[name='sexo'][value ='"+ dados[0].sexo + "']").prop('checked', true);
+  $('.sus').val(dados[0].sus)
+}
+function Limpar(){
+  $('.modal').find('input').val('')
+  $("input[name='diabetico']").prop('checked', false);
+  $("input[name='hipertenso']").prop('checked', false);
+  $("input[name='sexo']").prop('checked', false);
+}
+async function editando(id, vacina, remedios){
+  var data = $('.nascimento').val()
+  var newdata = data.split('-', 3)
+  var paciente = {nome:$('.nome').val(), nascimento:newdata[2]+'/'+newdata[1]+'/'+newdata[0], cpf:$('.cpf').val(), diabetico:$("input[name='diabetico']:checked").val(), hipertenso:$("input[name='hipertenso']:checked").val(), sexo:$("input[name='sexo']:checked").val(), sus:$('.sus').val(), medicamentosdeusocontinuo:remedios, dosesdevacina:vacina};
+  const response = await fetch(url +'/'+ id,{method:'PUT', body:JSON.stringify(paciente), headers:{'Content-Type': 'application/json'}})
+    if(!response.ok){
+        throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+    }
+    else{
+        console.log(`ok ${paciente}`)
+    }
+    $('.modal').css('display', 'none')
+
+}
--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const db = 'mongodb+srv://Cadpaciente:Cadpaciente/@cadastros.raucuy7.mongodb.net/?retryWrites=true&w=majority'
+const db = 'mongodb+srv://Cadpaciente:Cadpaciente@cadastros.raucuy7.mongodb.net/pacientes?retryWrites=true&w=majority'
 
 
-const connect = mongoose.connect(db);
+const connect = mongoose.connect(db)
 
 module.exports = connect
